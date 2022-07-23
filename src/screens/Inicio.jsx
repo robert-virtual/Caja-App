f2f6aa29dee33fad1f2c77a0d36d5b5ca23f360e
robert-virtual <robertocastill63@gmail.com>
2022-07-23 16:14:32 -0600

    navegacion

---
+++ src/screens/Inicio.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { FlatList, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Pressable } from "react-native-web";
+const pages = {
+  Monedas: "Monedas",
+  DetalleAperturaCaja: "Detalle Apertura Caja",
+  DetalleCierreCaja: "Detalle Cierre Caja",
+  DetalleCierreCajaPos: "Detalle Cierre CajaPos"
+}
+
+export function Inicio({navigation}) {
+  function navigate (page) {
+   navigation.navigate(page) 
+  }
+  return (
+    <FlatList 
+      data={Object.keys(pages)}
+      keyExtractor={(item)=> item}
+      renderItem={({item}) => (
+        <Pressable
+        onPress={()=>navigate(item)}
+        style={styles.item}>
+          <Text>{pages[item]}</Text>
+        </Pressable>
+      )}>
+
+    </FlatList>
+  )
+}
+const styles = StyleSheet.create({
+  item: {
+    padding: 10,
+    backgroundColor: "#fff",
+    margin: 5
+  },
+});
---
+++ src/screens/Menu.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Monedas } from './Monedas';
+import { DetalleAperturaCaja } from './DetalleAperturaCaja';
+import { DetalleCierreCaja } from './DetalleCierreCaja';
+import { DetalleCierreCajaPos } from './DetalleCierreCajaPos';
+import { Inicio } from './Inicio';
+const Stack = createNativeStackNavigator()
+
+export function Menu() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Inicio" component={Inicio} />
+      <Stack.Screen name="Monedas" component={Monedas} />
+      <Stack.Screen
+        name="DetalleAperturaCaja"
+        component={DetalleAperturaCaja} />
+      <Stack.Screen
+        name="DetalleCierreCaja"
+        component={DetalleCierreCaja} />
+      <Stack.Screen
+        name="DetalleCierreCajaPos"
+        component={DetalleCierreCajaPos} />
+    </Stack.Navigator>
+  );
+}
+
